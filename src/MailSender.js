9403e74b75a3573c61e11a53fd38eb0b0130f606
chelvyn20 <chelvyn20@gmail.com>
2021-08-12 00:36:58 +0700

    consumer program finished

---
+++ src/MailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Open Music App',
+      to: targetEmail,
+      subject: 'Export Playlist',
+      text: 'The attachment is exported from Open Music App Playlist',
+      attachments: [
+        {
+          filename: 'playlists.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
